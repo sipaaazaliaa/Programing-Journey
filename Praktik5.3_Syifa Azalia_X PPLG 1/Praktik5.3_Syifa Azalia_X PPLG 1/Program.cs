55cdd7c6f8acdfbc2b127e2fb75aa85c256fc0a6
sipaaazaliaa <azalia258735@student.smkn1kandeman.sch.id>
2025-11-24 15:11:42 +0700

    Add files via upload

---
+++ Praktik5.3_Syifa Azalia_X PPLG 1/Praktik5.3_Syifa Azalia_X PPLG 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik5._3_Syifa_Azalia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisiallisasi variabel untuk menyimpan input
+            String inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hinga anda mengetik 'keluar' .");
+
+            // Perulangan selama isi variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.Write("\nketik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari user
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+            }
+
+            Console.WriteLine("/nProgram selesai. Terima Kasih!");
+
+        }
+    }
+}
